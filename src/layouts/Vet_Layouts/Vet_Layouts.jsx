9efca403c4c11c9ea840f3203e687c0ee9281f67
Misael Gómez <gg20031@ues.edu.sv>
2023-05-18 16:30:44 -0600

    Cambios menores

--- src/layouts/Vet_Layouts/Vet_Layouts.jsx
+++ src/layouts/Vet_Layouts/Vet_Layouts.jsx
@@ -70,7 +70,7 @@ export function Vet_Layouts(props) {
       <Divider />
       <List>
       <ListItem disablePadding>
-            <ListItemButton component="a" href="/catalogo">
+            <ListItemButton component="a" href="">
               <ListItemIcon>
                 <PeopleAltIcon />
               </ListItemIcon>
@@ -79,7 +79,7 @@ export function Vet_Layouts(props) {
       </ListItem>
       <Divider />
       <ListItem disablePadding>
-            <ListItemButton component="a" href="/informacion">
+            <ListItemButton component="a" href="">
               <ListItemIcon>
                 <PetsIcon />
               </ListItemIcon>
@@ -90,7 +90,7 @@ export function Vet_Layouts(props) {
       <Divider />
       <List>
       <ListItem disablePadding>
-            <ListItemButton component="a" href="/contacto">
+            <ListItemButton component="a" href="">
               <ListItemIcon>
                 <CalendarMonthIcon />
               </ListItemIcon>
@@ -105,85 +105,92 @@ export function Vet_Layouts(props) {
     window !== undefined ? () => window().document.body : undefined;
   return (
     <>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          sx={{
-            width: { sm: `calc(100% - ${drawerWidth}px)` },
-            ml: { sm: `${drawerWidth}px` },
-          }}
-        >
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: "none" } }}
+    <div className="container">
+      <div>
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <AppBar
+            position="fixed"
+            sx={{
+              width: { sm: `calc(100% - ${drawerWidth}px)` },
+              ml: { sm: `${drawerWidth}px` },
+            }}
+          >
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: "none" } }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap component="div">
+                <Button 
+                variant="contained"
+                onClick={() => alert('LISTO')}
+                size="large"
+                className="my_button">Cerrar sesion</Button>
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Box
+            component="nav"
+            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+            aria-label="mailbox folders"
+          >
+            {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+            <Drawer
+              container={container}
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+              sx={{
+                display: { xs: "block", sm: "none" },
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  width: drawerWidth,
+                },
+              }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap component="div">
-              <Button className="button">Cerrar sesion</Button>
+              {drawer}
+            </Drawer>
+            <Drawer
+              variant="permanent"
+              sx={{
+                display: { xs: "none", sm: "block" },
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  width: drawerWidth,
+                },
+              }}
+              open
+            >
+              {drawer}
+            </Drawer>
+          </Box>
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              p: 3,
+              width: { sm: `calc(100% - ${drawerWidth}px)` },
+            }}
+          >
+            <Toolbar />
+            {children}
+            <Typography paragraph>
+              
             </Typography>
-          </Toolbar>
-        </AppBar>
-        <Box
-          component="nav"
-          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-          aria-label="mailbox folders"
-        >
-          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-          <Drawer
-            container={container}
-            variant="temporary"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-            sx={{
-              display: { xs: "block", sm: "none" },
-              "& .MuiDrawer-paper": {
-                boxSizing: "border-box",
-                width: drawerWidth,
-              },
-            }}
-          >
-            {drawer}
-          </Drawer>
-          <Drawer
-            variant="permanent"
-            sx={{
-              display: { xs: "none", sm: "block" },
-              "& .MuiDrawer-paper": {
-                boxSizing: "border-box",
-                width: drawerWidth,
-              },
-            }}
-            open
-          >
-            {drawer}
-          </Drawer>
+            <Typography paragraph></Typography>
+          </Box>
         </Box>
-        <Box
-          component="main"
-          sx={{
-            flexGrow: 1,
-            p: 3,
-            width: { sm: `calc(100% - ${drawerWidth}px)` },
-          }}
-        >
-          <Toolbar />
-          {children}
-          <Typography paragraph>
-            
-          </Typography>
-          <Typography paragraph></Typography>
-        </Box>
-      </Box>
-      
+      </div>
+    </div>
     </>
   );
 }

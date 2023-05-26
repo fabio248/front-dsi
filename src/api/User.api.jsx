e5e2bbc9e15af97478f36bb97b2751201910891e
Misael Gómez <gg20031@ues.edu.sv>
2023-05-25 23:11:52 -0600

    feat: add function to get a user by ID to API requests

--- src/api/User.api.jsx
+++ src/api/User.api.jsx
@@ -1,5 +1,30 @@
-import { ENV } from '../utils/Constanst';
+import { ENV } from '../utils';
+import { decoderToken } from '../utils';
 
 export class User {
-  
+  async getUser(accessToken) {
+    try {
+
+      const USER_ID = decoderToken(accessToken).identify;
+
+      //url de conexion con el backend      
+      const url = `${ENV.BASE_API}/${ENV.API_ROUTES.USERS_ID}/${USER_ID}`;
+      const params = {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+
+      const response = await fetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
 }

6f2884cc770205eef8dc4e79efa02403df18c6e6
Misael Gómez <gg20031@ues.edu.sv>
2023-05-28 11:32:12 -0600

    feat: protect vet routes

--- src/routers/Vet.routes.jsx
+++ src/routers/Vet.routes.jsx
@@ -3,27 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import { Auth_pages, Login } from "../pages";
 import { Vet_Layouts } from "../layouts";
 import { useAuth } from "../hooks";
+import { ProtectedRoute } from "../components/Admin/Auth/ProtectedRoutes";
 
 //const user = useAuth();
 
 export function Vet_routes() {
 
   const { user } = useAuth();
-  const [rol, setRol] = useState("default");
 
-  //console.log(user.role);
-  //console.log(user);
-
-  
-  useEffect(() => {
+  function isAdmin(){
     if (user != null) {
       const { role } = user.data;
-      console.log(role);
-      setRol(role);
-      //console.log(user.data.rol);
+      return role === "admin"
     }
-  }, [])
-  
+  }
+
   const Layout = (Layout, Pages) => {
     return (
       <>
@@ -35,21 +29,11 @@ export function Vet_routes() {
   };
   return (
     <Routes>
-
-      {/* validar un elemento user para verificar el acceso del usuario */}
-      {!user && rol != "admin"? (
-        <Route path="/admin/*" element={ <Login /> }/>
-        ) : (
-          <>
-          {/* cargar la misma ruta desde dos paths, con la funcion map de lodash*/}
-          { ["/admin", "/admin/auth"].map((path) => (
-            <Route key={path} path={path} element={Layout(Vet_Layouts, Auth_pages)}/>
-          ))}
-
-          {/* Rutas que accede el veterinaria al completar la autentificación */}
-          {/*<Route path="" element={Layout(Vet_Layouts, )}/>*/}
-          </>
-        )};
+      <Route path="/admin" element = {
+        <ProtectedRoute isAllowed={!!user && isAdmin()} redirectTo="/login">
+          {Layout(Vet_Layouts, Auth_pages)}
+        </ProtectedRoute>
+      }></Route>
     </Routes>
   );
 }

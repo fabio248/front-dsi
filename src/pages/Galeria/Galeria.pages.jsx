b98f669fc3fcf920c16bb38fa5188aca7d6a07e4
FyreFox360 <jmge5833@gmail.com>
2023-05-31 15:38:04 -0600

    feat: solved styles and optimizated

--- src/pages/Galeria/Galeria.pages.jsx
+++ src/pages/Galeria/Galeria.pages.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import IconButton from '@mui/material/IconButton';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
+import React from 'react';
 
 export function Galeria_pages() {
   return (
-    
     <div>
       {/* LightWidget WIDGET */}
-      <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
-      <iframe src="//lightwidget.com/widgets/992f8edcbd495aceb08b91bd6e808a19.html"
-      allowtransparency="true"
-      class="lightwidget-widget"
-      style={{ width: '100%', height: '500px', border: '0', overflow: 'hidden' }}></iframe>
+      <script src='https://cdn.lightwidget.com/widgets/lightwidget.js'></script>
+      <iframe
+        src='//lightwidget.com/widgets/992f8edcbd495aceb08b91bd6e808a19.html'
+        allowtransparency='true'
+        className='lightwidget-widget'
+        style={{
+          width: '100%',
+          height: '500px',
+          border: '0',
+          overflow: 'hidden',
+        }}
+      ></iframe>
     </div>
-  )
+  );
 }
--- src/pages/Historia/Historia.pages.jsx
+++ src/pages/Historia/Historia.pages.jsx
@@ -1,43 +1,62 @@
-import React from 'react'
-
+import React from 'react';
+import './Historia.pages.css';
 
 export function Historia_pages() {
   const { titulo, parrafos, mision, vision } = contenido;
   return (
-    
-    <div>
-       {/* LightWidget WIDGET */}
-       <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
-       <iframe src="//lightwidget.com/widgets/f6fd46029b3253689639b2126424e352.html"
-       allowtransparency="true" class="lightwidget-widget" 
-      style={{ width: '100%',height: '190px', border: '0', overflow: 'hidden' }}>
-      </iframe>
+    <div className='container'>
+      {/* LightWidget WIDGET */}
+      <script src='https://cdn.lightwidget.com/widgets/lightwidget.js'></script>
+      <iframe
+        src='//lightwidget.com/widgets/f6fd46029b3253689639b2126424e352.html'
+        allowtransparency='true'
+        className='lightwidget-widget'
+        style={{
+          width: '100%',
+          height: '190px',
+          border: '0',
+          overflow: 'hidden',
+        }}
+      ></iframe>
 
-      <h2 style={{ textAlign: 'center' }}>{titulo}</h2>
+      <h2>{titulo}</h2>
       {parrafos.map((parrafo, index) => (
-        <p key={index} style={{ textAlign: 'justify' }}>{parrafo}</p>
+        <p key={index} style={{ textAlign: 'justify' }}>
+          {parrafo}
+        </p>
       ))}
-   
-      <div style={{ textAlign: 'center' }}>
-      <img src="../../../public/galeria/huella.png" alt="Huella" style={{ width: '100%', height: 'auto' }}  />
+
+      <div>
+        <img
+          src='../../../public/galeria/huella.png'
+          alt='Huella'
+          style={{ width: '100%', height: 'auto' }}
+        />
       </div>
 
-      <h2 style={{ textAlign: 'center' }}>Misión</h2>
-      <p style={{ textAlign: 'center' }}>{mision}</p>
+      <h2>Misión</h2>
+      <p>{mision}</p>
 
-      <div style={{ textAlign: 'center' }}>
-      <img src="../../../public/galeria/huella.png" alt="Huella" style={{ width: '100%', height: 'auto' }}  />
+      <div>
+        <img
+          src='../../../public/galeria/huella.png'
+          alt='Huella'
+          style={{ width: '100%', height: 'auto' }}
+        />
       </div>
 
-      <h2 style={{ textAlign: 'center' }}>Visión</h2>
-      <p style={{ textAlign: 'center' }}>{vision}</p>
-      
-      <div style={{ textAlign: 'center' }}>
-      <img src="../../../public/galeria/cat.gif" alt="cat" style={{ width: '400px', height: 'auto' }}  />
+      <h2>Visión</h2>
+      <p>{vision}</p>
+
+      <div>
+        <img
+          src='../../../public/galeria/cat.gif'
+          alt='cat'
+          style={{ width: '400px', height: 'auto' }}
+        />
       </div>
-     
     </div>
-  )
+  );
 }
 
 const contenido = {
@@ -48,12 +67,12 @@ const contenido = {
     'El 3 de febrero de 2013 marcó un hito en nuestra historia, pues fue el día en que abrimos oficialmente nuestras puertas, listos para brindar servicios veterinarios de la más alta calidad a nuestra amada comunidad. Desde entonces, nos hemos dedicado a cuidar y proteger la salud de las mascotas, acompañando a sus dueños en cada paso del camino.',
     'En nuestra clínica veterinaria, nos enorgullece ofrecer atención personalizada, diagnósticos precisos y tratamientos efectivos, respaldados por el conocimiento y la experiencia del MVDr. Saul Antonio Medina Matus. Nuestro objetivo es garantizar el bienestar y la felicidad de cada mascota que entra por nuestras puertas, creando un vínculo duradero con sus dueños.',
     'En MISTUM, no solo tratamos a los animales, sino que también nos preocupamos profundamente por ellos. Los consideramos parte de nuestras familias y nos esforzamos por brindarles el amor, el respeto y la atención que se merecen.',
-    '¡Gracias por confiar en nosotros y ser parte de nuestra historia! Estamos ansiosos por servirles y ser su aliado confiable en el cuidado de veterinario sus adorables compañeros peludos.'
+    '¡Gracias por confiar en nosotros y ser parte de nuestra historia! Estamos ansiosos por servirles y ser su aliado confiable en el cuidado de veterinario sus adorables compañeros peludos.',
   ],
-  mision: 'Ser un establecimiento veterinario que proporcione servicios de atención Médico veterinarios a los propietarios dueños de mascotas que nos visitan.',
-  vision: 'Dar atención veterinaria a animales de compañía o mascotas, para que manteniendo su salud física o ayudar a recuperar la salud perdida, mantengamos así la salud pública veterinaria, y ayudemos proporcionar mejor la calidad de vida a nuestros pacientes.',
+  mision:
+    'Ser un establecimiento veterinario que proporcione servicios de atención Médico veterinarios a los propietarios dueños de mascotas que nos visitan.',
+  vision:
+    'Dar atención veterinaria a animales de compañía o mascotas, para que manteniendo su salud física o ayudar a recuperar la salud perdida, mantengamos así la salud pública veterinaria, y ayudemos proporcionar mejor la calidad de vida a nuestros pacientes.',
 };
 
 export default contenido;
-
-

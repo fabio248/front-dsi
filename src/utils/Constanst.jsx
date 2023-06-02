91b0c02b856e968335cc458b9da1d50e8cc60c2a
Misael Gómez <gg20031@ues.edu.sv>
2023-06-02 02:10:45 -0600

    style: add comments to required file to connect to API

--- src/utils/Constanst.jsx
+++ src/utils/Constanst.jsx
@@ -1,10 +1,10 @@
 const SERVER_IP = 'localhost:3000';
 
-//se crea este documento para que todos cuando ejecuten alguna peticion y la ruta no exista
-//solo se tenga que modificar la variable que se invoque
+// Se crea este documento para que todos cuando ejecuten alguna peticion y la ruta no exista
+// Solo se tenga que modificar la variable que se invoque
 export const ENV = {
-  BASE_PATH: `http://${SERVER_IP}`, //url que renderiza la app
-  BASE_API: `http://${SERVER_IP}/api/v1`, //esta url cambiará segun como se defina en el back
+  BASE_PATH: `http://${SERVER_IP}`, // URL que renderiza la app
+  BASE_API: `http://${SERVER_IP}/api/v1`, // Esta url cambiará segun como se defina en el back
 
   API_ROUTES: {
     //Todo esto seran los mismos nombres de las rutas del back segun como se defina
--- src/utils/Token.jsx
+++ src/utils/Token.jsx
@@ -1,9 +1,11 @@
 import jwt_decode from "jwt-decode";
 
+// Decodifica un token
 export function decoderToken(token){
     return jwt_decode(token);
 }
 
+// Verifica expiración de token
 export function hasExpiredToken(token){
 
     const exp = decoderToken(token);

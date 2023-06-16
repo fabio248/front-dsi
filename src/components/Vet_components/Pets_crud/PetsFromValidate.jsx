2a63a49c89e050e3400f76167a77a03f55ba42af
Misael Gómez <gg20031@ues.edu.sv>
2023-06-16 09:31:20 -0600

    fix: pet form validations

--- src/components/Vet_components/Pets_crud/PetsFromValidate.jsx
+++ src/components/Vet_components/Pets_crud/PetsFromValidate.jsx
@@ -5,37 +5,38 @@ import { isValid } from 'date-fns';
 export function initialPetValues(pet) {
   let newBirthday, dateObject;
   if (pet) {
-    newBirthday = pet.birthday.split('T');
-    newBirthday = format(new Date(newBirthday[0]), 'yyyy-MM-dd');
-    dateObject = parse(newBirthday, 'yyyy-MM-dd', new Date());
+    newBirthday = pet.birthday.split('T')[0];
+    const [year, month, day ] = newBirthday.split('-');
+    newBirthday = `${day}/${month}/${year}`;
+    dateObject = parse(newBirthday, 'dd/MM/yyyy', new Date());
   }
 
   return {
     name: pet?.name || '',
-    specie: pet?.species || undefined,
+    specie: pet?.species || null,
     raza: pet?.raza || '',
     color: pet?.color || '',
-    isHaveTattoo: pet?.isHaveTatto || undefined,
+    isHaveTattoo: pet?.isHaveTatto || false,
     birthday: pet? dateObject: null,
     gender: pet?.gender || '',
-    pedigree: pet?.pedigree || undefined,
+    pedigree: pet?.pedigree || false,
     //medicalHistory: {
       //  food: {
           quantityFood: pet?.medicalHistory?.food?.quantity || '',
           typeFood: pet?.medicalHistory?.food?.type || '',
       //  },
       descendencia: pet?.medicalHistory.descendants || '',
-      reproduccion: pet?.medicalHistory.isReproduced || undefined,
-      vacuna: pet?.medicalHistory?.isHaveAllVaccine || undefined,
+      reproduccion: pet?.medicalHistory.isReproduced || false,
+      vacuna: pet?.medicalHistory?.isHaveAllVaccine || false,
       //  otherPet: {
-          convivencia: pet?.medicalHistory?.otherPet?.isLiveOtherPets || undefined,
+          convivencia: pet?.medicalHistory?.otherPet?.isLiveOtherPets || false,
           whichPets: pet?.medicalHistory?.otherPet?.whichPets || '',
       //  },
       enfermedad: pet?.medicalHistory.diasesEvaluation || '',
       observacion: pet?.medicalHistory?.observation || '',
       habitaculo: pet?.medicalHistory.room || '',
     //  physicalExam: {
-        weight: pet?.medicalHistory?.physicalExam?.weight || 0,
+        weight: pet?.medicalHistory?.physicalExam?.weight || undefined,
         palpitaciones: pet?.medicalHistory?.physicalExam?.palpitations || '',
     //  },
     //},
@@ -46,9 +47,7 @@ export function validationSchemaPetRegister(pet) {
   return yup.object({
     name: yup.string().required('El nombre es obligatorio'),
     specie: yup
-    .string()
-    .min(1)
-    .max(7)
+    .object()
     .required('El campo de especie solo acepta las especies listadas'),
     raza: yup.string().required('La raza es obligatoria'),
     color: yup.string().required('El color de pelaje es obligatorio'),
@@ -89,13 +88,15 @@ export function validationSchemaPetRegister(pet) {
     .string()
     .when('convivencia', {
       is: true,
-      then: () => yup.string().required('Si convive con tras mascotas, ¿Cuáles mascotas?'),
+      then: () => yup.string().required('Se verificó que convive con otras mascotas, por lo tanto este campo ahora es requerido'),
       otherwise: () => yup.string(),
     }),
     enfermedad: yup.string().required('Los detalles de la enfermedad o falta de ella son requeridos'),
     observacion: yup.string().required('Las observaciones son obligatorias'),
     habitaculo: yup.string().required('El habitáculo es obligatorio'),
-    weight: yup.number().required('El peso de la mascota es obligatorio'),
+    weight: yup.number()
+    .positive('El peso debe ser positivo para que sea válido')
+    .required('El peso de la mascota es obligatorio'),
     palpitaciones: yup.string().required('Las palpitaciones obligatorio'),
   });
 }

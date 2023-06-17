02a96a3dd62b10f5b597520cec702ef3c00bccde
Claudia María Argueta Caneza <91162449+ClaudiaArguetaCaneza@users.noreply.github.com>
2023-06-16 22:19:33 -0600

    feat: Auth pages Admin

--- src/pages/Auth_pages/Auth.pages.jsx
+++ src/pages/Auth_pages/Auth.pages.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { Typography, Grid } from '@mui/material';
 
 export function Auth_pages(props) {
   const { children } = props;
 
   return (
     <>
-      <h1>Bienvenido a la pagina administradora</h1>
+      <Typography
+        className='Auth-page_add'
+        variant='h4'
+        component='h1'
+        color='#69458C'
+      >
+        ¡Bienvenido a Clínica Veterinaria MISTUM!
+      </Typography>
 
-      <h2>homsidnmoaqnsdin</h2>
+      <Grid container spacing={2} justifyContent="center" style={{ marginBottom: '20px', marginTop: '20px' }}>
+        <Grid item xs={5}>
+          <img
+            src='../../../public/galeria/favicon.png'
+            alt='logo'
+            style={{ width: '90%', height: 'auto' }}
+          />
+        </Grid>
+      </Grid>
+
+      <Typography color='#8EC167' textAlign="center">
+        <h2>SALUD PARA TU MASCOTA</h2>
+      </Typography>
       {children}
     </>
   );

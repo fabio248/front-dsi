66b0fe644ba2fbb02125d680760cf53cca7e6225
FyreFox360 <jmge5833@gmail.com>
2023-06-17 14:14:11 -0600

    fix: solved error Typography

--- src/components/Vet_components/Pets_crud/ListeredAllPets/ListeredAllPets.jsx
+++ src/components/Vet_components/Pets_crud/ListeredAllPets/ListeredAllPets.jsx
@@ -9,7 +9,9 @@ import { User } from '../../../../api/User.api';
 import { PetsAllItems } from '../PetsAllItems';
 
 //mui material
-import { CircularProgress, Typography } from '@mui/material';
+import { CircularProgress } from '@mui/material';
+import Typography from '@mui/material';
+
 import { size, map } from 'lodash';
 
 //clase Pets

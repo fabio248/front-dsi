5d610ba2f281bf598b0e44a84a52d737f7c37536
Misael Gómez <90297880+MisaelGG11@users.noreply.github.com>
2023-09-28 15:52:02 -0600

    feat: create medical history mapping

---
+++ src/pages/Vet_pages/PerfilPets/MedicalHistory.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+
+// mui material elements
+import ListItemText from '@mui/material/ListItemText';
+import { Divider, Avatar, Grid, IconButton } from '@mui/material';
+import ListItem from '@mui/material/ListItem';
+import PetsIcon from '@mui/icons-material/Pets';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemAvatar';
+import List from '@mui/material/List';
+import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
+import VaccinesIcon from '@mui/icons-material/Vaccines';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { size, map } from 'lodash';
+
+const defaultTheme = createTheme();
+
+export function PetMedicalHistory({ medicalHistory }) {
+  //Modal informacion concreta
+  const [showVisualizar, setShowVisualizar] = useState(false);
+  const onOpenInfoClientAndPets = () =>
+    setShowVisualizar((prevState) => !prevState);
+
+  const [titleSeeInfoClientAndPet, setTitleSeeInfoClientAndPet] = useState('');
+
+  //ejecuta la funcion de visualizacion de informacion de cliente y su mascota (VisibilityIcon)
+  const openInfoClientAndPets = () => {
+    setTitleSeeInfoClientAndPet(`Datos específicos de la mascota`);
+    onOpenInfoClientAndPets();
+  };
+  return (
+    <ThemeProvider theme={defaultTheme}>
+    <div>
+      <ListItem
+        sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}
+      >
+        <Avatar sx={{ mx: 4, width: 50, height: 50, bgcolor: '#8EC167' }}>
+          <HistoryEduIcon sx={{ fontSize: 40 }} />
+        </Avatar>
+        <ListItemText>
+          <br />
+          <b
+            className='estilos-pets'
+            style={{ justifyContent: 'space-around' }}
+          >
+            <b>Fecha de registro: </b>
+            <span style={{ color: 'gray' }}>{medicalHistory.createdAt}</span>
+            <br />
+            <b>Diagnostico: </b>
+            <span style={{ color: 'gray' }}>{medicalHistory.diagnostic.description}</span>
+            <br />
+            <b>Tratamiento: </b>
+            <br/>
+            <span style={{ color: 'gray' }}>
+            <List dense={true} sx={{ m: 0, p:0 }}>
+            { map(medicalHistory.diagnostic.treatments, (medication) => (
+                <ListItem key={medication.id} sx={{ m: 0, ml:2, p:0 }}>
+                    <ListItemIcon>
+                        <VaccinesIcon sx={{ color:'#8EC167' }} />
+                    </ListItemIcon>
+                    <ListItemText
+                        sx={{ m:0, ml: -3, p:0 }}
+                        primary={medication.name}
+                        secondary={medication.frequency}
+                    />
+                </ListItem>
+            ))}
+            </List>
+            </span>
+            <b>Intervención quirúrgica: </b>
+            <span style={{ color: 'gray' }}>
+            {!medicalHistory.diagnostic.surgicalIntervations ? 'No ha tenido' 
+            : map(medicalHistory.diagnostic.surgicalIntervations, (surgicalIntervation) => (
+                <ListItem key={surgicalIntervation.id} sx={{ m: 0, ml:2, p:0 }}>
+                    <ListItemIcon>
+                        <LocalHospitalIcon sx={{ color:'#8EC167' }} />
+                    </ListItemIcon>
+                    <ListItemText
+                        sx={{ m:0, ml: -3, p:0 }}
+                        primary={surgicalIntervation.description}
+                        secondary={surgicalIntervation.intervationDate}
+                    />
+                </ListItem>
+            ))
+            }</span>
+            <br />
+          </b>
+        </ListItemText>
+        <ListItemAvatar>
+          <Grid container justifyContent='flex-end'>
+            <Grid item>
+                <IconButton color='info'>
+                  <VisibilityIcon sx={{ fontSize: 30 }} />
+                </IconButton>
+              <IconButton color='warning' >
+                <ModeEditIcon sx={{ fontSize: 30 }} />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </ListItemAvatar>
+      </ListItem>
+      <Divider>
+        <PetsIcon color='disabled' />
+      </Divider>
+    </div>
+    </ThemeProvider>
+  );
+}

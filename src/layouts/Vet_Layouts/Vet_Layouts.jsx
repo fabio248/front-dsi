80ff601cceb9994f1a0ae289d1a439c6dd2a2eeb
fabio248 <fabioflores021@gmail.com>
2023-10-29 14:06:48 -0600

    refactor: revert layout vet

--- src/layouts/Vet_Layouts/Vet_Layouts.jsx
+++ src/layouts/Vet_Layouts/Vet_Layouts.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Vet_Layouts.css';
-import { useAuth } from '../../hooks/UseAuth';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -17,11 +16,12 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import CategoryIcon from '@mui/icons-material/Category';
+import DescriptionIcon from '@mui/icons-material/Description';
 import EventIcon from '@mui/icons-material/Event';
 import { Logout } from '../../components/Admin/Auth/Logout';
 import { NavLink } from 'react-router-dom';
-import PetsIcon from "@mui/icons-material/Pets";
-import {ProductionQuantityLimitsTwoTone} from "@mui/icons-material";
 
 const drawerWidth = 240;
 
@@ -34,191 +34,210 @@ export function Vet_Layouts(props) {
     setMobileOpen(!mobileOpen);
   };
   const drawer = (
-    <div>
-      <div className='image-container'>
-        <img src='/galeria/logo.png' alt='logo' className='imagen-appbar' />
-      </div>
+      <div>
+        <div className='image-container'>
+          <img src='/galeria/logo.png' alt='logo' className='imagen-appbar' />
+        </div>
 
-      <Divider sx={{ mt: -3 }} />
-      <List>
-        <NavLink
-          to='/admin/users'
-          style={{
-            textDecoration: 'none', // Quitar subrayado
-            color: 'black', // Color de texto deseado
-          }}
-        >
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <PeopleAltIcon />
-              </ListItemIcon>
-              <ListItemText primary='Clientes' />
-            </ListItemButton>
-          </ListItem>
-        </NavLink>
-        <Divider />
-
-        <NavLink
-          to='/admin/calendar'
-          style={{
-            textDecoration: 'none', // Quitar subrayado
-            color: 'black', // Color de texto deseado
-          }}
-        >
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <EventIcon />
-              </ListItemIcon>
-              <ListItemText primary='Programación de citas' />
-            </ListItemButton>
-          </ListItem>
-        </NavLink>
-        <Divider />
-        <NavLink
-          to='/admin/userAndPets'
-          style={{
-            textDecoration: 'none', // Quitar subrayado
-            color: 'black', // Color de texto deseado
-          }}
-        >
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <PetsIcon />
-              </ListItemIcon>
-              <ListItemText primary='Mascotas' />
-            </ListItemButton>
-          </ListItem>
-        </NavLink>
-        <Divider />
-
-        <Divider />
-        <NavLink
-          to='/admin/products'
-          style={{
-            textDecoration: 'none', // Quitar subrayado
-            color: 'black', // Color de texto deseado
-          }}
-        >
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <ProductionQuantityLimitsTwoTone/>
-              </ListItemIcon>
-              <ListItemText primary='Productos' />
-            </ListItemButton>
-          </ListItem>
-        </NavLink>
-        <Divider />
-        <ListItem disablePadding>
-          <ListItemButton
-              href='https://calendar.google.com/calendar/u/0/r'
-              target='_blank'
+        <Divider sx={{ mt: -3 }} />
+        <List>
+          <NavLink
+              to='/admin/users'
+              style={{
+                textDecoration: 'none', // Quitar subrayado
+                color: 'black', // Color de texto deseado
+              }}
           >
-            <ListItemIcon>
-              <CalendarMonthIcon />
-            </ListItemIcon>
-            <ListItemText primary='Visualizar citas' />
-          </ListItemButton>
-        </ListItem>
-        <Divider />    
-      </List>
-    </div>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <PeopleAltIcon />
+                </ListItemIcon>
+                <ListItemText primary='Clientes' />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+          <Divider />
+
+          <NavLink
+              to='/admin/calendar'
+              style={{
+                textDecoration: 'none', // Quitar subrayado
+                color: 'black', // Color de texto deseado
+              }}
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <EventIcon />
+                </ListItemIcon>
+                <ListItemText primary='Programación de citas' />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+
+          <Divider />
+          <ListItem disablePadding>
+            <ListItemButton
+                href='https://calendar.google.com/calendar/u/0/r'
+                target='_blank'
+            >
+              <ListItemIcon>
+                <CalendarMonthIcon />
+              </ListItemIcon>
+              <ListItemText primary='Visualizar citas' />
+            </ListItemButton>
+          </ListItem>
+
+          <Divider />
+          <NavLink
+              to='/admin/userAndPets'
+              style={{
+                textDecoration: 'none', // Quitar subrayado
+                color: 'black', // Color de texto deseado
+              }}
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <PersonAddAltIcon />
+                </ListItemIcon>
+                <ListItemText primary='Agregar Clientes y sus mascotas' />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+          <Divider />
+
+          <Divider />
+          <NavLink
+              to='/admin/products'
+              style={{
+                textDecoration: 'none', // Quitar subrayado
+                color: 'black', // Color de texto deseado
+              }}
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <CategoryIcon/>
+                </ListItemIcon>
+                <ListItemText primary='Agregar Productos' />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+          <Divider/>
+          <NavLink
+              to='/admin/facturations'
+              style={{
+                textDecoration: 'none', // Quitar subrayado
+                color: 'black', // Color de texto deseado
+              }}
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <DescriptionIcon/>
+                </ListItemIcon>
+                <ListItemText primary='Facturación' />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+          <Divider />
+        </List>
+      </div>
   );
   const container =
-    window !== undefined ? () => window().document.body : undefined;
+      window !== undefined ? () => window().document.body : undefined;
   return (
-    <>
-      <div className='container'>
-        <div>
-          <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar
-              position='fixed'
-              sx={{
-                width: { sm: `calc(100% - ${drawerWidth}px)` },
-                ml: { sm: `${drawerWidth}px` },
-              }}
-            >
-              <Toolbar>
-                <IconButton
-                  color='inherit'
-                  aria-label='open drawer'
-                  edge='start'
-                  onClick={handleDrawerToggle}
-                  sx={{ mr: 2, display: { sm: 'none' } }}
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Typography variant='h6' noWrap component='div'>
-                  <div
-                    style={{
-                      position: 'absolute',
-                      right: '10px',
-                      bottom: '15px',
-                    }}
+      <>
+        <div className='container'>
+          <div>
+            <Box sx={{ display: 'flex' }}>
+              <CssBaseline />
+              <AppBar
+                  position='fixed'
+                  sx={{
+                    width: { sm: `calc(100% - ${drawerWidth}px)` },
+                    ml: { sm: `${drawerWidth}px` },
+                  }}
+              >
+                <Toolbar>
+                  <IconButton
+                      color='inherit'
+                      aria-label='open drawer'
+                      edge='start'
+                      onClick={handleDrawerToggle}
+                      sx={{ mr: 2, display: { sm: 'none' } }}
                   >
-                    <Logout />
-                  </div>
-                </Typography>
-              </Toolbar>
-            </AppBar>
-            <Box
-              component='nav'
-              sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-              aria-label='mailbox folders'
-            >
-              {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-              <Drawer
-                container={container}
-                variant='temporary'
-                open={mobileOpen}
-                onClose={handleDrawerToggle}
-                ModalProps={{
-                  keepMounted: true, // Better open performance on mobile.
-                }}
-                sx={{
-                  display: { xs: 'block', sm: 'none' },
-                  '& .MuiDrawer-paper': {
-                    boxSizing: 'border-box',
-                    width: drawerWidth,
-                  },
-                }}
+                    <MenuIcon />
+                  </IconButton>
+                  <Typography variant='h6' noWrap component='div'>
+                    <div
+                        style={{
+                          position: 'absolute',
+                          right: '10px',
+                          bottom: '15px',
+                        }}
+                    >
+                      <Logout />
+                    </div>
+                  </Typography>
+                </Toolbar>
+              </AppBar>
+              <Box
+                  component='nav'
+                  sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+                  aria-label='mailbox folders'
               >
-                {drawer}
-              </Drawer>
-              <Drawer
-                variant='permanent'
-                sx={{
-                  display: { xs: 'none', sm: 'block' },
-                  '& .MuiDrawer-paper': {
-                    boxSizing: 'border-box',
-                    width: drawerWidth,
-                  },
-                }}
-                open
+                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                <Drawer
+                    container={container}
+                    variant='temporary'
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                      keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                      display: { xs: 'block', sm: 'none' },
+                      '& .MuiDrawer-paper': {
+                        boxSizing: 'border-box',
+                        width: drawerWidth,
+                      },
+                    }}
+                >
+                  {drawer}
+                </Drawer>
+                <Drawer
+                    variant='permanent'
+                    sx={{
+                      display: { xs: 'none', sm: 'block' },
+                      '& .MuiDrawer-paper': {
+                        boxSizing: 'border-box',
+                        width: drawerWidth,
+                      },
+                    }}
+                    open
+                >
+                  {drawer}
+                </Drawer>
+              </Box>
+              <Box
+                  component='main'
+                  sx={{
+                    flexGrow: 1,
+                    p: 3,
+                    width: { sm: `calc(100% - ${drawerWidth}px)` },
+                  }}
               >
-                {drawer}
-              </Drawer>
+                <Toolbar />
+                {children}
+                <Typography paragraph></Typography>
+                <Typography paragraph></Typography>
+              </Box>
             </Box>
-            <Box
-              component='main'
-              sx={{
-                flexGrow: 1,
-                p: 3,
-                width: { sm: `calc(100% - ${drawerWidth}px)` },
-              }}
-            >
-              <Toolbar />
-              {children}
-              <Typography paragraph></Typography>
-              <Typography paragraph></Typography>
-            </Box>
-          </Box>
+          </div>
         </div>
-      </div>
-    </>
+      </>
   );
 }

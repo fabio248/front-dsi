3e0099d632a931f907c79198e945fb5cda851338
MisaelGG11 <90297880+MisaelGG11@users.noreply.github.com>
2023-11-02 21:08:23 -0600

    fix: amazon query

--- src/api/Pets.api.jsx
+++ src/api/Pets.api.jsx
@@ -125,7 +125,7 @@ export class Pets {
       const response = await fetch(url, params);
       const result = await response.json();
 
-      if (response.status !== 200) throw result;
+      if (response.status !== 201) throw result;
       return result;
     } catch (error) {
       throw error;

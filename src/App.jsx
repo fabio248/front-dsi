007b334d141dc807616176f644298b0d81a12414
fabioflores <fabioflores021@gmail.com>
2024-08-10 09:30:18 -0600

    feat: add seed insights

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import {
 } from './routers';
 import { AuthProvider } from './context';
 import { BrowserRouter } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './App.css';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+        <SpeedInsights />
         <Auth_routes />
         <Clients_routes />
         <Vet_routes />
